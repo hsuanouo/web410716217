80586071cd9d7ebe9cbc952411401813a2e0ea08
李妘翾 <gggggg51396@gmail.com>
2021-05-20 21:09:10 +0800

    HW1

---
+++ Web410716217/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410716217.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+        public ActionResult HW2()
+        {
+            return View();
+        }
+}
